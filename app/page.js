06fad13bcb9b2ba914fe2e15a349003170527bde
alexsaussier <asaussier99@gmail.com>
2024-10-16 20:37:31 +0100

    m

--- app/page.js
+++ app/page.js
@@ -105,7 +105,6 @@ export default function Home() {
       {/* Activities Section */}
       <section className="py-16">
             <div className="container mx-auto px-4">
-
               <h2 className="text-3xl font-bold mb-8 text-center transition-transform duration-300">Exciting Activities for Everyone</h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {activities.map((activity, index) => (
@@ -122,7 +121,9 @@ export default function Home() {
                       </div>
                       <div className="p-6">
                         <h3 className="text-xl font-semibold mb-2 transition-colors duration-300 group-hover:text-blue-600">{activity.name}</h3>
-                        <p className="text-lg transition-opacity duration-300 group-hover:opacity-80">{activity.description}</p>
+                        <p className="text-lg transition-opacity duration-300 group-hover:opacity-80 line-clamp-3">
+                          {activity.description}
+                        </p>
                       </div>
                     </div>
                   </Link>

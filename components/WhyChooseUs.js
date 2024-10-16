1dd65e9125bbfd6ab3713633edd49c3b4751af0c
alexsaussier <asaussier99@gmail.com>
2024-10-16 20:49:42 +0100

    m

--- components/WhyChooseUs.js
+++ components/WhyChooseUs.js
@@ -1,8 +1,10 @@
+import MarlinPhoto from '../public/images/marlin-photo.jpg';
+
 export default function WhyChooseUs() {
   return (
-    <section id="about" className="py-16 bg-gray-800 text-white">
+    <section id="about" className="py-16 bg-gray-200 text-white">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-12">Why Choose Us?</h2>
+        <h2 className="text-4xl font-bold text-center text-gray-800 mb-12">Why Choose Us?</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Reason cards */}
           <ReasonCard
@@ -10,20 +12,20 @@ export default function WhyChooseUs() {
             description="Experience the thrill of catching majestic Blue Marlins in one of the world&apos;s premier fishing destinations."
           />
           <ReasonCard
-            title="Luxurious Accommodations"
-            description="Relax in comfort with our top-notch lodging facilities, designed to provide the perfect retreat after a day on the water."
+            title="Diverse Activities"
+            description="From spa treatments to beach excursions, we offer a wide range of activities for both anglers and non-fishing guests."
           />
           <ReasonCard
             title="Expert Guides and Staff"
             description="Our experienced team ensures you have the best possible fishing experience, with knowledge passed down through generations."
           />
           <ReasonCard
-            title="Unforgettable Culinary Experiences"
-            description="Savor exquisite local and international cuisine prepared by our skilled chefs using the freshest ingredients."
+            title="Luxurious Accommodations"
+            description="Relax in comfort with our top-notch lodging facilities, designed to provide the perfect retreat after a day on the water."
           />
           <ReasonCard
-            title="Diverse Activities"
-            description="From spa treatments to beach excursions, we offer a wide range of activities for both anglers and non-fishing guests."
+            title="Unforgettable Culinary Experiences"
+            description="Savor exquisite local and international cuisine in the best locations of the area."
           />
           <ReasonCard
             title="Breathtaking Location"
@@ -37,9 +39,9 @@ export default function WhyChooseUs() {
 
 function ReasonCard({ title, description }) {
   return (
-    <div className="bg-gray-700 p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300">
-      <h3 className="text-xl font-semibold mb-4 text-red-400">{title}</h3>
-      <p>{description}</p>
+    <div className="bg-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition border-2 hover:border-blue-800 duration-300">
+      <h3 className="text-2xl font-semibold mb-4 text-gray-800">{title}</h3>
+      <p className="text-lg text-gray-600">{description}</p>
     </div>
   );
 }

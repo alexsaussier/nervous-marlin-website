a84466ad9256232e8bec4b6b675d3029dbc20b97
Alexandre Saussier <asaussier99@gmail.com>
2024-12-04 15:39:27 +0000

    fixes

--- components/Backlinks.js
+++ components/Backlinks.js
@@ -6,6 +6,7 @@ const Backlinks = () => {
       <link rel="canonical" href="https://nervousmarlin.com" />
       <meta name="related-projects" content="https://etudes-canada.com, https://finterpret.co, https://batterseawebdesign.com" />
       {/* You can add more meta tags or links here */}
+      
     </Head>
   );
 };
